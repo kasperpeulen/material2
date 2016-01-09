092296bad0b8edfcb0d16a4ae0c7bf6529542d32
Kasper Peulen <kasperpeulen@gmail.com>
2016-01-09 01:58:16 +0100

    use boostrap library for example

--- example/main.dart
+++ example/main.dart
@@ -1,4 +1,6 @@
-import 'package:angular2/platform/browser.dart';
+import 'package:angular2/bootstrap.dart';
 import 'package:material2/demo_app/demo_app.dart';
 
-main() => bootstrap(DemoApp);
+main() {
+  bootstrap(DemoApp);
+}
